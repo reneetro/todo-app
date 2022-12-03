6732f107008f326bff99eff232cde0438e18f746
Renee Troiani <renee.troiani@gmail.com>
2022-12-03 12:11:34 -0700

    added Task model

---
+++ src/models/Task.js
@@ -0,0 +1,9 @@
+export class Task{
+    constructor(id, title, listName, completed, notes) {
+      this.id = id;
+      this.title = title;
+      this.listName = listName;
+      this.completed = completed;
+      this.notes = notes;
+    }
+  }
